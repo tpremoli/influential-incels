bf81007bd315a4f3beac759bd25b9c05f81b5e4c
tpremoli <tomas@premoli.org>
2024-03-13 12:51:41 +0000

    added weights to graph calculation

--- analyze.py
+++ analyze.py
@@ -19,39 +19,39 @@ print("adding users (nodes) to graph")
 for user in tqdm(users, desc="Processing users"):
     G.add_node(user['user_id'], username=user['username'])
 
-# Add edges (interactions) to the graph
-print("adding edges (interactions) to graph")
-for post in tqdm(posts, desc="Processing posts"):
-    # Add an edge for the post itself (if the author is replying to someone)
-    if post['mentioned_users']:
-        for mentioned_user in post['mentioned_users']:
-            G.add_edge(post['user_id'], mentioned_user)
+# Define weights for different types of interactions
+mention_quote_weight = 1.0
+general_comment_weight = 0.5
 
-    # Add edges for each comment
+# Add edges (interactions) to the graph
+print("Adding edges (interactions) to graph...")
+for post in tqdm(posts, desc="Processing posts"):
     for comment in post['comments']:
-        # if we don't mention or quote anyone, we consider it an interaction with the original user
-        # NOTE: we'll evaluate if we want to do this. Maybe include different weights for
-        # general comments vs direct interactions?
-        if not comment['mentioned_users'] and not comment['quoted_posts']:
-            if comment['user_id'] != post['user_id']:
-                G.add_edge(comment['user_id'], post['user_id'])
-            continue
-        
+        # Direct interaction through mentions or quotes
+        has_direct_interaction = False
+
         # If the comment mentions other users
         if comment['mentioned_users']:
+            has_direct_interaction = True
             for mentioned_user in comment['mentioned_users']:
-                G.add_edge(comment['user_id'], mentioned_user)
+                G.add_edge(comment['user_id'], mentioned_user, weight=mention_quote_weight)
 
         # If the comment quotes other posts
-        for quoted_post in comment['quoted_posts']:
-            # Find the original poster and add an edge
-            original_poster = next((item['user_id'] for item in posts if item['post_id'] == quoted_post), None)
-            if original_poster:
-                G.add_edge(comment['user_id'], original_poster)
+        if comment['quoted_posts']:
+            has_direct_interaction = True
+            for quoted_post in comment['quoted_posts']:
+                # Find the original poster and add an edge
+                original_poster = next((item['user_id'] for item in posts if item['post_id'] == quoted_post), None)
+                if original_poster:
+                    G.add_edge(comment['user_id'], original_poster, weight=mention_quote_weight)
+
+        # General comment interaction with the original post's author
+        if not has_direct_interaction and comment['user_id'] != post['user_id']:
+            G.add_edge(comment['user_id'], post['user_id'], weight=general_comment_weight)
 
 # Apply the PageRank algorithm
 print("applying pagerank algorithm to graph")
-pagerank = nx.pagerank(G)
+pagerank = nx.pagerank(G, weight='weight')
 
 # Sort users by their PageRank scores
 print("sorting users by pagerank scores")
